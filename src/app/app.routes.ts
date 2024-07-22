cf1679a1003bba9e1a9e40d8a407d9e85908c328
SergioGonzaMarti <sergio.gonza.marti@gmail.com>
2024-07-22 09:59:00 -0500

    Integracion con rama develop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
-import { PermisosComponent } from './components/permisos/permisos.component';
 import { ContentComponent } from './components/content/content.component';
 
 export const routes: Routes = [
 
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'permisos', component: PermisosComponent },
     {
         path: 'usuarios',
         component: ContentComponent,
--- src/app/components/modules/usuarios/usuarios-routing.module.ts
+++ src/app/components/modules/usuarios/usuarios-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearContactoComponent } from './crear-contacto/crear-contacto.component';
+import { PermisosComponent } from './permisos/permisos.component';
 
 const routes: Routes = [
   { path: 'crear_contacto', component: CrearContactoComponent },
+  { path: 'permisos', component: PermisosComponent },
 ];
 
 @NgModule({
