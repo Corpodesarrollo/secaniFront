2afd637e6bd2b88b54b6f9ba372c210d52efa9ce
SergioGonzaMarti <sergio.gonza.marti@gmail.com>
2024-08-05 19:26:18 -0500

    Se corrige el formato de las tablas y filtros (Permisos - Consultar EAPB)

--- src/app/components/modules/usuarios/eapb/eapb.component.ts
+++ src/app/components/modules/usuarios/eapb/eapb.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit, ViewChild  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ModalCrearComponent } from './modal-crear/modal-crear.component';
+import { TableModule } from 'primeng/table';
 
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-eapb',
   standalone: true,
-  imports: [ModalCrearComponent, CommonModule, FormsModule],
+  imports: [ModalCrearComponent, CommonModule, FormsModule, TableModule],
   templateUrl: './eapb.component.html',
   styleUrl: './eapb.component.css'
 })
@@ -29,20 +30,22 @@ export class EAPBComponent implements OnInit {
     { eapb: 'EPS Cafam', nombreApe: 'Felipe Arias', cargo: 'Jefe de Doctores', telefono: '3208987516', correo: 'luz3@sanitas.com', estado: 'Activo' },
   ];
 
+  originalData: any[] = [...this.data];
+
   selectedItem: any = null;
   isEditing: boolean = false;
 
-  pageSize: number = 5;
-  currentPage: number = 1;
-  paginatedData: any[] = [];
-  searchTerm: string = '';
-  selectedArea: string = '';
+  filtroBuscar: string = '';
+  filtroEAPB: string = '';
+
+  first = 0;
+  rows = 10;
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.updatePaginatedData();
-  }
+  ngOnInit(): void {  }
+
+  /**Modal Crear y Editar**/
 
   onEdit(item: any) {
     this.selectedItem = item;
@@ -62,64 +65,68 @@ export class EAPBComponent implements OnInit {
     }
   }
 
-  updatePaginatedData(): void {
-    const filteredData = this.filterData(this.searchTerm, this.selectedArea);
-    const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = startIndex + this.pageSize;
-    this.paginatedData = filteredData.slice(startIndex, endIndex);
+  /**Filtros**/
+
+  limpiar() {
+    this.filtroEAPB = "";
+    this.filtroBuscar = "";
+    this.data = [...this.originalData];
   }
 
-  filterData(searchTerm: string, selectedArea: string): any[] {
-    let filteredData = this.data;
-
-    if (searchTerm) {
-      searchTerm = searchTerm.toLowerCase();
-      filteredData = filteredData.filter(item =>
+  buscar(filtroBuscar: string, filtroEAPB: string) {
+    this.data = [...this.originalData];
+    
+    if (filtroBuscar) {
+      filtroBuscar = filtroBuscar.toLowerCase();
+      this.data = this.data.filter(item =>
         Object.values(item).some(value => {
           if (typeof value === 'string') {
-            return value.toString().toLowerCase().includes(searchTerm);
+            return value.toString().toLowerCase().includes(filtroBuscar);
           }
           return false;
         })
       );
     }
 
-    if (selectedArea) {
-      filteredData = filteredData.filter(item => item.eapb === selectedArea);
-    }
-
-    return filteredData;
-  }
-
-  nextPage(): void {
-    const filteredData = this.filterData(this.searchTerm, this.selectedArea);
-    if ((this.currentPage * this.pageSize) < filteredData.length) {
-      this.currentPage++;
-      this.updatePaginatedData();
+    if (filtroEAPB) {
+      this.data = this.data.filter(item => item.eapb === filtroEAPB);
     }
   }
 
-  previousPage(): void {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.updatePaginatedData();
-    }
+  onFiltroBuscarChange(): void {
+    //console.log('Buscar cambiado:', event);
+    this.buscar(this.filtroBuscar,this.filtroEAPB); // Llama a la función de búsqueda
   }
 
-  onSearchTermChange(): void {
-    this.currentPage = 1; // Reset to first page on search
-    this.updatePaginatedData();
+  onFiltroEAPBChange(): void {
+    //console.log('Orden cambiado:', event);
+    this.buscar(this.filtroBuscar,this.filtroEAPB); // Llama a la función de búsqueda
   }
 
-  onAreaChange(): void {
-    this.currentPage = 1; // Reset to first page on area change
-    this.updatePaginatedData();
+  /**Paginador**/
+  next() {
+    this.first = this.first + this.rows;
   }
 
-  clearFilters(): void {
-    this.searchTerm = '';
-    this.selectedArea = '';
-    this.currentPage = 1;
-    this.updatePaginatedData();
+  prev() {
+    this.first = this.first - this.rows;
   }
+
+  reset() {
+    this.first = 0;
+  }
+
+  pageChange(event: any) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
+
+  isLastPage(): boolean {
+    return this.data ? this.first === this.data.length - this.rows : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.data ? this.first === 0 : true;
+  }
+
 }
