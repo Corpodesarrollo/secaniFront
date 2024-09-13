1ca2a41a8d336f26b65de9307cdb94084b80c481
SergioGonzaMarti <sergio.gonza.marti@gmail.com>
2024-09-12 23:24:55 -0500

    Se agrega ruta de prueba en environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,14 +8,9 @@ export const environment = {
   url_MsAuthention: 'https://localhost:7084/', 
   url_MSEntidad: 'https://localhost:7174/',
   url_MSPermisos: 'https://mspermisos-arbtcacfbcb2akes.eastus2-01.azurewebsites.net/api/',
-  url_MsAuthention: 'https://localhost:7084/',
   url_MsNna: 'https://localhost:7291/',
   url_MSSeguimiento: 'https://localhost:7085/',
-  url_MSEntidad: 'https://localhost:7174',
-  url_MSPermisos: 'https://localhost:7020',
-
   url_Parametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/api/',
-
   url_MSTablasParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/api/',
   //url_MsNna: 'https://msnna-bjdgg4h5b0duh0gz.eastus-01.azurewebsites.net/',
   //url_MSSeguimiento: 'https://msseguimientov2-byb0d5gwh9c9hufx.westus2-01.azurewebsites.net/',
