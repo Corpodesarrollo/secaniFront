a9f0d82dc8d03e6b3c802909798aecce72ce93ea
jairo93 <80946044+jairo93@users.noreply.github.com>
2024-09-21 09:19:19 -0500

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,6 @@ export const environment = {
   url_MsAuthention: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
   url_MSEntidad: 'https://msentidad-bhh8bufwd4gngead.eastus2-01.azurewebsites.net/',
   url_MSPermisos: 'https://mspermisos-arbtcacfbcb2akes.eastus2-01.azurewebsites.net/api/',
-  //url_MsNna: 'https://localhost:7291/',
-  url_MSSeguimiento: 'https://msseguimientov2-byb0d5gwh9c9hufx.westus2-01.azurewebsites.net/',
   url_Parametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/api/',
   url_MSTablasParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/api/',
   url_MsNna: 'https://msnna-bjdgg4h5b0duh0gz.eastus-01.azurewebsites.net/',
