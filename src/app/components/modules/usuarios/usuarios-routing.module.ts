d9ab0032f7fb8b1c0234a89e352cab18a5bb4a4b
jairo93 <80946044+jairo93@users.noreply.github.com>
2024-10-17 20:40:22 -0500

    Terminando desarrollo
    se implementa historia de usuario rq01-hu03 detalle nna

--- src/app/components/modules/usuarios/usuarios-routing.module.ts
+++ src/app/components/modules/usuarios/usuarios-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { EAPBComponent } from './eapb/eapb.component';
 import { HistoricoNnaComponent } from './nna/historico-nna/historico-nna.component';
 import { CrearNnaComponent } from './nna/crear-nna/crear-nna.component';
+import { DetalleNnaComponent } from './nna/detalle-nna/detalle-nna.component';
 
 const routes: Routes = [
   { path: 'consultar_eapb', component:  EAPBComponent},
   { path: 'historico_nna', component: HistoricoNnaComponent },
   { path: 'crear_nna', component: CrearNnaComponent },
+  { path: 'detalle_nna/:idNna', component: DetalleNnaComponent },
 ];
 
 @NgModule({
--- src/app/components/modules/usuarios/usuarios.module.ts
+++ src/app/components/modules/usuarios/usuarios.module.ts
@@ -7,8 +7,13 @@ import { TableModule } from 'primeng/table';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+import { AccordionModule } from 'primeng/accordion';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
 
 import { CrearNnaComponent } from './nna/crear-nna/crear-nna.component';
+import { DetalleNnaComponent } from './nna/detalle-nna/detalle-nna.component';
 
 
 import { BotonNotificacionComponent } from '../boton-notificacion/boton-notificacion.component';
@@ -20,12 +25,12 @@ import { DialogCrearContactoComponent } from './nna-contacto/dialog-crear-contac
 import { BrowserModule } from '@angular/platform-browser';
 import { CrearNnaAgregarContactoComponent } from './nna-contacto/crear-nna-agregar-contacto/crear-nna-agregar-contacto.component';
 import { RadioButtonModule } from 'primeng/radiobutton';
-import { CalendarModule } from 'primeng/calendar';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -52,6 +57,11 @@ import { InputIconModule } from 'primeng/inputicon';
     FloatLabelModule,
     IconFieldModule,
     InputIconModule,
+    CardModule,
+    AccordionModule,
+    SelectButtonModule,
+    DropdownModule,
+    CalendarModule,
 
     /**Component standalone */
     BotonNotificacionComponent,
