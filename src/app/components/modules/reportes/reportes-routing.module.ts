ec52a275a01f9c9d1b6d0a19fd5b04ad0f7539d4
Kamilo1205 <kamilo_1205@outlook.com>
2024-11-20 13:00:51 -0500

    feat: Mover la pagina de reporte de depuracion al modulo de reportes

--- src/app/components/modules/reportes/reportes-routing.module.ts
+++ src/app/components/modules/reportes/reportes-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ReporteInconsistenciaComponent } from './reporte-inconsistencia/reporte-inconsistencia.component';
+import { ReporteDepuracionComponent } from './reporte-depuracion/reporte-depuracion.component';
 
 const routes: Routes = [
   { path: 'reporte-inconsistencia', component: ReporteInconsistenciaComponent },
+  {
+    path: 'reporte-depuracion',
+    children: [
+      { path: '', component: ReporteDepuracionComponent },
+      { path: '**', redirectTo: '' }
+    ]
+  },
   { path: '**', redirectTo: 'reporte-inconsistencia' }
 ];
 
