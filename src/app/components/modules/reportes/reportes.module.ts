bd76392f8b760a7864b419a599bf1e988109e07f
Kamilo1205 <kamilo_1205@outlook.com>
2024-11-20 10:18:35 -0500

    feat: Agregar modulo de rutas al módulo de reportes

--- src/app/components/modules/reportes/reportes.module.ts
+++ src/app/components/modules/reportes/reportes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReportesRoutingModule } from './reportes-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
+    ReportesRoutingModule,
     CommonModule
   ]
 })
