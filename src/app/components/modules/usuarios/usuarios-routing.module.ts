d0c0249cc292a80e6614e5bd9694d770dd2af4db
jairo93 <80946044+jairo93@users.noreply.github.com>
2024-11-27 02:24:13 -0500

    rq07-hu08 ninos pendientes por reportar sivigila

--- src/app/components/modules/usuarios/usuarios-routing.module.ts
+++ src/app/components/modules/usuarios/usuarios-routing.module.ts
@@ -6,6 +6,7 @@ import { CrearNnaComponent } from './nna/crear-nna/crear-nna.component';
 import { DetalleNnaComponent } from './nna/detalle-nna/detalle-nna.component';
 import { EditarNnaComponent } from './nna/editar-nna/editar-nna.component';
 import { CasosTerritorioComponent } from './casos-territorio/casos-territorio.component';
+import { PendienteReportarComponent } from './nna/pendiente-reportar/pendiente-reportar.component';
 
 const routes: Routes = [
   { path: 'consultar_eapb', component:  EAPBComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'crear_nna', component: CrearNnaComponent },
   { path: 'detalle_nna/:idNna', component: DetalleNnaComponent },
   { path: 'editar_nna/:idNna', component: EditarNnaComponent },
-  { path: 'casos-territorio', component: CasosTerritorioComponent }
+  { path: 'casos-territorio', component: CasosTerritorioComponent },
+  { path: 'pendiente-reportar', component: PendienteReportarComponent }
 ];
 
 @NgModule({
