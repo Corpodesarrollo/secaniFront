f8ef51037b7294d30baafcf9e6987d8dbb27ce01
Kamilo1205 <kamilo_1205@outlook.com>
2024-12-04 17:57:18 -0500

    feat: corregir rutas y agregar modal de confirmacion de eliminacion

--- src/app/components/modules/administracion/plantillas-correo/plantillas-correo.component.ts
+++ src/app/components/modules/administracion/plantillas-correo/plantillas-correo.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ConfirmationService } from 'primeng/api';
 
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { DialogModule } from 'primeng/dialog';
 import { TableModule } from 'primeng/table';
 
@@ -10,15 +12,18 @@ import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-plantillas-correo',
   standalone: true,
-  imports: [ButtonModule, CommonModule, DialogModule, RouterModule, TableModule],
+  imports: [ButtonModule, ConfirmDialogModule, CommonModule, DialogModule, RouterModule, TableModule],
   templateUrl: './plantillas-correo.component.html',
-  styleUrl: './plantillas-correo.component.css'
+  styleUrl: './plantillas-correo.component.css',
+  providers: [ConfirmationService]
 })
 export class PlantillasCorreoComponent implements OnInit {
 
   public plantillasCorreo: any[] = [];
   public plantillaCorreoSeleccionada: any | null = null;
   public mostrarDetallesModal: boolean = false;
+
+  constructor(private confirmationService: ConfirmationService) {}
 
   ngOnInit(): void {
     this.plantillasCorreo = [
@@ -38,5 +43,25 @@ export class PlantillasCorreoComponent implements OnInit {
   closeViewModal(): void {
     this.mostrarDetallesModal = false;
     this.plantillaCorreoSeleccionada = null;
+  }
+
+  confirm(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: '¿Estás seguro de que quieres eliminar la plantilla de correo?',
+      header: 'Confirmar eliminación',
+      icon: 'none',
+      acceptButtonStyleClass: "p-button-danger p-button-text",
+      rejectButtonStyleClass: "p-button-secondary p-button-text",
+      acceptIcon: "none",
+      rejectIcon: "none",
+      accept: () => {
+          // Acción al aceptar
+      },
+      reject: () => {
+          // Acción al rechazar
+      }
+    });
+
   }
 }
