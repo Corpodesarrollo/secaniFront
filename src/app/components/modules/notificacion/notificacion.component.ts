7de9915778aa44b53986339146616fe07c395b4d
charlesrock96 <charlesrock96@yahoo.com>
2024-12-04 20:14:11 -0500

    commit 20241204

--- src/app/components/modules/notificacion/notificacion.component.ts
+++ src/app/components/modules/notificacion/notificacion.component.ts
@@ -17,6 +17,8 @@ import { ButtonModule } from 'primeng/button';
 import { TablasParametricas } from '../../../core/services/tablasParametricas';
 import { TpParametros } from '../../../core/services/tpParametros';
 import { Plantilla } from '../../../models/plantilla.model';
+import { GenericService } from '../../../services/generic.services';
+import { apis } from '../../../models/apis.model';
 
 @Component({
   selector: 'app-notificacion',
@@ -53,7 +55,7 @@ export class NotificacionComponent {
 
   notificacion: Notificacion = {
     id: 0,
-    idEntidad: 0,
+    idEntidad: '',
     para: [],
     conCopia: [],
     plantillaId: 0,
@@ -61,11 +63,13 @@ export class NotificacionComponent {
     mensaje: "",
     agregarComentario: false,
     comentario: "",
-    adjunto: "",
+    adjunto: null,
     firma: "",
   };
 
-  constructor(private messageService: MessageService, private tp: TablasParametricas, private tpp: TpParametros) {}
+   mostrarDialogo: boolean = false;
+
+  constructor(private messageService: MessageService, private tp: TablasParametricas, private tpp: TpParametros, private repos: GenericService,) {}
 
   async ngOnInit(): Promise<void> {
     this.entidades =  await this.tp.getTP('EntidadTerritorial');
@@ -73,6 +77,91 @@ export class NotificacionComponent {
 
     this.plantillas = await this.tpp.getPlantillas();
     this.isLoadingPLantillas = false;
+  }
+
+  async enviar(){
+    this.notificacion.idEntidad = this.selectedEntidad?.codigo ?? '';
+    this.notificacion.plantillaId = this.selectedPlantilla?.id ?? 0;
+
+    this.submitted = true;
+    if (this.notificacion.para.length === 0) {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Debe ingresar al menos un correo.' });
+      return;
+    }
+
+    if (this.notificacion.idEntidad === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Debe seleccionar una entidad.' });
+      return;
+    }
+
+    if (this.notificacion.asunto === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Debe ingresar un asunto.' });
+      return;
+    }
+
+    if (this.notificacion.mensaje === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Debe ingresar un mensaje.' });
+      return;
+    }
+
+    if (this.notificacion.agregarComentario && this.notificacion.comentario === '') {
+      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Debe ingresar un comentario.' });
+      return;
+    }
+
+    // Enviar notificación
+
+    if (this.selectedFile) {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const fileContent = fileReader.result as ArrayBuffer;
+
+        this.notificacion.adjunto = {
+          fileName: this.selectedFile?.name ?? null,
+          fileExtension: this.selectedFile?.name.split('.').pop() ?? null,
+          file: Array.from(new Uint8Array(fileContent))
+        };
+
+        console.log(this.notificacion);
+
+        this.guardar();
+      };
+
+      fileReader.readAsArrayBuffer(this.selectedFile);
+    } else {
+      this.guardar();
+    }
+  }
+
+  guardar(){
+    this.repos.post("Notificacion/EnviarOficioNotificacion", this.notificacion, apis.seguimiento).subscribe({
+      next: (response) => {
+        this.mostrarDialogo = true;
+      },
+      error: (error) => {
+        console.error('Error al consumir el API:', error);
+      }
+    });
+  }
+
+  limpiar(){
+    this.notificacion = {
+          id: 0,
+          idEntidad: '',
+          para: [],
+          conCopia: [],
+          plantillaId: 0,
+          asunto: "",
+          mensaje: "",
+          agregarComentario: false,
+          comentario: "",
+          adjunto: null,
+          firma: "",
+        };
+
+    this.showDialog = false;
+    this.selectedPlantilla = undefined;
+    this.selectedEntidad = undefined;
   }
 
   openModal() {
