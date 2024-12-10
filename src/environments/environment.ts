cf7bc305a87fc8905012389cef448d1a0b49e944
Kamilo1205 <kamilo_1205@outlook.com>
2024-12-10 15:14:13 -0500

    feat: conectar al back el reporte dinámico de seguimiento

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-/* export const environment = {
+export const environment = {
   cookie : false,
   production: false,
   url: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/swagger/index.html',
@@ -22,7 +22,7 @@
   url_MSSeguimiento: 'https://msseguimientov2-byb0d5gwh9c9hufx.westus2-01.azurewebsites.net/',
   url_MSParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/api/',
   url_MSUsuarioyRoles: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
-}; */
+};
 
 
 /*
@@ -42,17 +42,17 @@ export const environment = {
 
 
 */
-export const environment = {
-  cookie : false,
-  production: false,
-  url: 'http://192.168.110.12/auth/',
-  url_MsAuthention: 'http://192.168.110.12/auth/',
-   url_MSEntidad: 'http://192.168.110.12/auth/',
-   url_MSPermisos: 'http://192.168.110.12/auth/',
-   url_Parametricas: 'http://192.168.110.12/tablas/',
-   url_MSTablasParametricas: 'http://192.168.110.12/tablas/',
-   url_MsNna: 'http://192.168.110.12/nna/',
-   url_MSSeguimiento: 'http://192.168.110.12/seguimiento/',
-   url_MSParametricas: 'http://192.168.110.12/tablas/',
-   url_MSUsuarioyRoles: 'http://192.168.110.12/auth/',
-};
+// export const environment = {
+//   cookie : false,
+//   production: false,
+//   url: 'http://192.168.110.12/auth/',
+//   url_MsAuthention: 'http://192.168.110.12/auth/',
+//    url_MSEntidad: 'http://192.168.110.12/auth/',
+//    url_MSPermisos: 'http://192.168.110.12/auth/',
+//    url_Parametricas: 'http://192.168.110.12/tablas/',
+//    url_MSTablasParametricas: 'http://192.168.110.12/tablas/',
+//    url_MsNna: 'http://192.168.110.12/nna/',
+//    url_MSSeguimiento: 'http://192.168.110.12/seguimiento/',
+//    url_MSParametricas: 'http://192.168.110.12/tablas/',
+//    url_MSUsuarioyRoles: 'http://192.168.110.12/auth/',
+// };
