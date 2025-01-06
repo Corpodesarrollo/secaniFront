230adeea13db088264374542275be0005b0545e8
Kamilo1205 <kamilo_1205@outlook.com>
2025-01-05 19:10:37 -0500

    feat: crear servicios de plantillas de correo

--- src/app/services/plantillas-correo.service.ts
+++ src/app/services/plantillas-correo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { Generico } from '../core/services/generico';
 import { environment } from '../../environments/environment';
 
@@ -8,7 +9,22 @@ export class PlantillasCorreoService {
   constructor(private generico: Generico) { }
 
   crearEditarPlantillaCorreo(data: any) {
-    const url: string = `Notificacion/OficioNotificacion`;
+    const url: string = `CrearPlantillaCorreo`;
     return this.generico.retorno_post(url, data, true, this.urlbase);
+  }
+
+  obtenerPlantillasCorreo() {
+    const url: string = `ConsultarPlantillasCorreo`;
+    return this.generico.retorno_get(url, this.urlbase);
+  }
+
+  historioPlantillaCorreo(id: string) {
+    const url: string = `HistoricoPlantillaCorreo/${id}`;
+    return this.generico.retorno_get(url, this.urlbase);
+  }
+
+  obtnenerPlantillaCorreoPorId(id: string) {
+    const url: string = `ConsultarPlantillaCorreo/${id}`;
+    return this.generico.retorno_get(url, this.urlbase);
   }
 }
