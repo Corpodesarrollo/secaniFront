39a5b40421813820c202a6ca621118bc00d3444d
Kamilo1205 <kamilo_1205@outlook.com>
2025-01-05 19:52:40 -0500

    feat: conectar servicio para obtener historico, crear y actualizar una plantilla

--- src/app/components/modules/administracion/nueva-plantilla-correo/nueva-plantilla-correo.component.ts
+++ src/app/components/modules/administracion/nueva-plantilla-correo/nueva-plantilla-correo.component.ts
@@ -1,32 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+import { MessageService } from 'primeng/api';
 
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { EditorModule } from 'primeng/editor';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 import { PlantillasCorreoService } from '../../../../services/plantillas-correo.service';
 
 @Component({
   selector: 'app-nueva-plantilla-correo',
   standalone: true,
-  imports: [CommonModule, ButtonModule, DropdownModule, EditorModule, InputTextModule, InputTextareaModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ButtonModule, DropdownModule, EditorModule, InputTextModule, InputTextareaModule, ReactiveFormsModule, RouterModule, ToastModule],
   templateUrl: './nueva-plantilla-correo.component.html',
-  styleUrl: './nueva-plantilla-correo.component.css'
+  styleUrl: './nueva-plantilla-correo.component.css',
+  providers: [MessageService],
 })
 export class NuevaPlantillaCorreoComponent {
+
   public tiposPlantillas: any[] = [];
   public firmantes: any[] = [];
   public estados: any[] = [];
 
+  public plantillaId: string | null = null;
   public plantillaCorreoForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private plantillasCorreoService: PlantillasCorreoService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private plantillasCorreoService: PlantillasCorreoService,
+    private route: ActivatedRoute,
+    private messageService: MessageService
+  ) {
     this.plantillaCorreoForm = this.formBuilder.group({
+      id: [''],
       nombre: ['', [Validators.required,  Validators.maxLength(100)]],
       tipoPlantilla: ['', Validators.required],
       firmante: ['', Validators.required],
@@ -35,6 +47,21 @@ export class NuevaPlantillaCorreoComponent {
       mensaje: ['', [Validators.required, Validators.maxLength(10000)]],
       cierre: ['', [Validators.required, Validators.maxLength(1000)]]
     });
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id) {
+        this.plantillaId = id; // Guarda el id
+        this.obtenerPlantillaPorId(id); // Carga los datos de la plantilla si estamos editando
+      }
+    });
+  }
+
+  async obtenerPlantillaPorId(id: string) {
+    const datos = await this.plantillasCorreoService.obtnenerPlantillaCorreoPorId(id);
+    if (datos) this.plantillaCorreoForm.patchValue(datos);
   }
 
   isValidField( field: string ): boolean | null {
@@ -63,7 +90,9 @@ export class NuevaPlantillaCorreoComponent {
       return this.plantillaCorreoForm.markAllAsTouched();;
 
     const data = this.plantillaCorreoForm.value;
-    const response = await this.plantillasCorreoService.crearEditarPlantillaCorreo(data);
+    await this.plantillasCorreoService.crearEditarPlantillaCorreo(data);
+
+    this.messageService.add({ severity: 'success', summary: 'Éxito', detail: 'Plantilla de correo creada o actualizada correctamente' });
     this.plantillaCorreoForm.reset();
   }
 }
