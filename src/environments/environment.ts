2e376ef4a9cbbdbd5a01eb6d5309095c059f3a7f
charlesrock96 <charlesrock96@yahoo.com>
2025-04-07 23:54:03 -0500

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,37 +4,37 @@
 
 // RUTAS DE DESARROLLO
 
-export const environment = {
-  cookie : false,
-  production: false,
-  url: 'https://localhost:7084/',
-  url_MsAuthention: 'https://localhost:7084/',
-  url_MSEntidad: 'https://localhost:7084/',
-  url_MSPermisos: 'https://localhost:7084/',
-  url_Parametricas: 'https://localhost:7294/',
-  url_MSTablasParametricas: 'https://localhost:7294/',
-  url_MsNna: 'https://localhost:7291/',
-  url_MSSeguimiento: 'https://localhost:7085/',
-  url_MSParametricas: 'https://localhost:7294/',
-  url_MSUsuarioyRoles: 'https://localhost:7084/',
-};
-
-
-// RUTAS DE AZURE
 // export const environment = {
 //   cookie : false,
 //   production: false,
-//   url: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
-//   url_MsAuthention: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
-//   url_MSEntidad: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
-//   url_MSPermisos: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
-//   url_Parametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/',
-//   url_MSTablasParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/',
-//   url_MsNna: 'https://msnna-bjdgg4h5b0duh0gz.eastus-01.azurewebsites.net/',
-//   url_MSSeguimiento: 'https://msseguimientov2-byb0d5gwh9c9hufx.westus2-01.azurewebsites.net/',
-//   url_MSParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/',
-//   url_MSUsuarioyRoles: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
+//   url: 'https://localhost:7084/',
+//   url_MsAuthention: 'https://localhost:7084/',
+//   url_MSEntidad: 'https://localhost:7084/',
+//   url_MSPermisos: 'https://localhost:7084/',
+//   url_Parametricas: 'https://localhost:7294/',
+//   url_MSTablasParametricas: 'https://localhost:7294/',
+//   url_MsNna: 'https://localhost:7291/',
+//   url_MSSeguimiento: 'https://localhost:7085/',
+//   url_MSParametricas: 'https://localhost:7294/',
+//   url_MSUsuarioyRoles: 'https://localhost:7084/',
 // };
+
+
+// RUTAS DE AZURE
+export const environment = {
+  cookie : false,
+  production: false,
+  url: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
+  url_MsAuthention: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
+  url_MSEntidad: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
+  url_MSPermisos: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
+  url_Parametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/',
+  url_MSTablasParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/',
+  url_MsNna: 'https://msnna-bjdgg4h5b0duh0gz.eastus-01.azurewebsites.net/',
+  url_MSSeguimiento: 'https://msseguimientov2-byb0d5gwh9c9hufx.westus2-01.azurewebsites.net/',
+  url_MSParametricas: 'https://mstablasparametricas-bdf0a9cza5bucwby.eastus2-01.azurewebsites.net/',
+  url_MSUsuarioyRoles: 'https://msauthentication-auc6a9ajccerbndk.eastus2-01.azurewebsites.net/',
+};
 
 // RUTAS DE MINISTERIO
 // export const environment = {
