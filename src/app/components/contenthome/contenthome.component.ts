ba47fa8a25fff0a47d0ae4e60545f6139eadf462
camiloJ <camilo12052000@hotmail.com>
2025-09-10 18:14:43 -0500

    feat: agregar vista de content home

--- src/app/components/contenthome/contenthome.component.ts
+++ src/app/components/contenthome/contenthome.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TarjetaCabeceraComponent } from '../modules/shared/tarjeta-cabecera/tarjeta-cabecera.component';
 
 @Component({
   selector: 'app-contenthome',
   templateUrl: './contenthome.component.html',
-  styleUrls: ['./contenthome.component.css']
+  styleUrls: ['./contenthome.component.css'],
+  standalone: true,
+  imports: [TarjetaCabeceraComponent]
 })
 
-export class ContenthomeComponent implements OnInit {
+export class ContenthomeComponent {
   //TODO:  SE DEBE IMPLEMENTAR LAS REGLAS PARA EL MANEJO DE LOS ROLES DE USUARIO
+  usuario: any = "Juan Manuel";
   rol: any = 1;
   constructor(public router: Router) { }
 
@@ -23,6 +27,6 @@ export class ContenthomeComponent implements OnInit {
     if(this.rol == 3) {
       this.router.navigate(['dashboard-eapb'], { skipLocationChange: true });
     }
-  }
+  } 
 
 }
